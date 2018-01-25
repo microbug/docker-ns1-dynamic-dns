d07c30cf8915c76a71c04368d4456d75716b3c65
Richard Coleman <richard@microbug.uk>
2018-01-25 23:20:45 +0000

    Make logs make more sense

--- dynamic-dns.py
+++ dynamic-dns.py
@@ -15,12 +15,12 @@ def check_ip(record):
     my_ip = requests.get("https://api.ipify.org/?format=json").json()["ip"]
     dns_ip = record.data['answers'][0]['answer'][0]
     if my_ip == dns_ip:
-        log_print("Current IP matches DNS record for {record} (IP={ip})"
+        log_print("Current IP ({ip}) matches DNS record for {record}"
                   .format(record=record.domain, ip=my_ip))
         return {'matches': True}
     else:
-        log_print("Current IP does not match DNS record for {record} (IP={ip})"
-                  .format(record=record.domain, ip=my_ip))
+        log_print("Current IP ({my_ip}) does not match DNS record for {record} (record IP={dns_ip})"
+                  .format(record=record.domain, my_ip=my_ip, dns_ip=dns_ip))
         return {'matches': False, 'my_ip': my_ip}
 
 
